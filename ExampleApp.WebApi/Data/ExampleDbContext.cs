e3f68a7eb25a29ae29754f3662cd6aef69ba523f
Juraj Doncevic <juraj.doncevic@fer.hr>
2024-03-10 18:30:55 +0100

    cleaned usings

--- ExampleApp.WebApi/Data/ExampleDbContext.cs
+++ ExampleApp.WebApi/Data/ExampleDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using ExampleApp.WebApi.Models;
+﻿using ExampleApp.WebApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExampleApp.WebApi.Data;
